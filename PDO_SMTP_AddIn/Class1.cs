4d74d7648a05b999ebd696bebe1a904eecbd2e38
NickOetjen <oetjen@pdoetjen.de>
2023-10-17 10:19:25 +0200

    Create some fault tolerance

--- PDO_SMTP_AddIn/Class1.cs
+++ PDO_SMTP_AddIn/Class1.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Net;
 using System.Net.Mail;
 
@@ -10,37 +6,36 @@ namespace PDO_SMTP_AddIn
 {
     public class SendMail_using_SMTP
     {
-        // PDO: Als COM-Addin erstellt. Kann dann als AddIn in VBA eingebunden und genutzt werden.
+        // PDO: Configured as COM-Addin. Can be used as AddIn in VBA , after installation and inclusion as reference.
 
-        // Hintergrund: Von VBA aus wird Stand 2023 meist die cdo-Bibliothek genutzt, welche allerdings
-        // von neueren Exchange-Servern nicht mehr unterstützt wird.
+        // Background: From VBA status quo 2023 mostly the cdo-library is used, which is 
+        // deprecated / not supported by newer Exchange-Servers (>= 2019).
 
         // GUID:  0b37731e-4fe7-4b76-826b-ce34e7d5b5aa
 
         private string Version = "20231016";
-        private string strResult;
+        private string strResult = "";
 
 
 
-        // E-Mail-Konfiguration
-        private MailMessage mail = new MailMessage();
+        // E-Mail-Configuration
+        private MailMessage mail = new MailMessage();   // new MailMessage(emailFrom, emailTo, subject, body);
         private string SetsmtpServer = "";
-        //private int    SetsmtpPort = 587; // ohne ssl:  465; // 587;  Wird unten anhand SetIsSSL gesetzt
+        //private int    SetsmtpPort = 587; // ohne ssl:  465; // 587;  Will be set using SetIsSSL 
         private bool SetIsSSL = true;
         private string SetPassword = "";
 
         private string SetEmailFrom = "";  // default, sollte bei Aufruf überschrieben werden
-        //private string SetemailTo = "nick@pdoetjen.de";    // Über Methode To_Add
         
-        private string SetSubject = "Neue Test-E-Mail";
-        private string Sethtmlbody = "Dies ist eine Test-E-Mail von mir .";
+        private string SetSubject = "New Test-E-Mail";
+        private string Sethtmlbody = "Can I <string>test</strong> you?";
 
 
 
-        // Werte setzen
-        public void smtpServer(string strSMTPServer)
+        // Methods to set values
+        public void smtpServer(string str)
         {
-            SetsmtpServer = strSMTPServer;
+            SetsmtpServer = str;
         }
 
         public void IsSSL(bool boo)
@@ -53,9 +48,9 @@ namespace PDO_SMTP_AddIn
             SetPassword = str;
         }
 
-        public void Subject(string strSubject)
+        public void Subject(string str)
         {
-            SetSubject = strSubject;
+            SetSubject = str;
         }
 
         public void EmailFrom(string str)
@@ -65,17 +60,37 @@ namespace PDO_SMTP_AddIn
 
         public void To_Add(string str)
         {
-            mail.To.Add(str);
+            if (IsValidEmail(str))
+            {
+                mail.To.Add(str);
+            } else
+            {
+                strResult = "PDO_SMTP_AddIn: The recipient-address is invalid. Please check.";
+            }
         }
 
         public void CC_Add(string str)
         {
-            mail.CC.Add(str);
+            if (IsValidEmail(str))
+            {
+                mail.CC.Add(str);
+            }
+            else
+            {
+                strResult = "PDO_SMTP_AddIn: The CC-address is invalid. Please check.";
+            }
         }
 
         public void BCC_Add(string str)
         {
-            mail.Bcc.Add(str);
+            if (IsValidEmail(str))
+            {
+                mail.Bcc.Add(str);
+            }
+            else
+            {
+                strResult = "PDO_SMTP_AddIn: The Bcc-address is invalid. Please check.";
+            }
         }
 
         public void HTMLBody(string str)
@@ -91,65 +106,98 @@ namespace PDO_SMTP_AddIn
 
 
 
-        // Nur ein Test, ob die Verbindung zum COM steht
+        // Just to check for connection to COM 
         public string HelloWorld()
         {
             return "Hello World, this is PDO_SMTP_AddIn Version " + Version;
         }
 
 
+        // Main function
         public string SendMail()
         {
 
 
-            // E-Mail erstellen, einige Werte hier, mit default, nicht über Methode
-            // MailMessage mail = new MailMessage(emailFrom, emailTo, subject, body);
+            // Configure Mail with some defaults, not using the methods, to make it more tolerant to faults
+            
+            if (string.IsNullOrEmpty(SetEmailFrom))
+            {
+                throw new Exception("PDO_SMTP_AddIn: The sender-address must not be empty. Please check.");
+                
+            }
+
             mail.From = new MailAddress(SetEmailFrom);
-            //mail.To.Add (SetemailTo);
+
+            //mail.To.Add (SetEmailTo);
             //mail.CC.Add(Mail_CC);
             mail.Subject = SetSubject;
             mail.Body = Sethtmlbody;
             mail.IsBodyHtml = true;
 
-            /*Für Attachments*/
-            //System.Net.Mail.Attachment attachment;
-            //attachment = new System.Net.Mail.Attachment(item);
-            //mail.Attachments.Add(attachment);
 
+            // SMTP-Client-object create and configure
+            if (string.IsNullOrEmpty(SetsmtpServer))
+            {
+                throw new Exception("PDO_SMTP_AddIn: The smtpServer must not be empty. Please check.");
 
-            // SMTP-Client erstellen und konfigurieren
+            }
+
             SmtpClient smtpClient = new SmtpClient(SetsmtpServer);
-            smtpClient.Port =  ( SetIsSSL ) ? 587 : 465 ;//     SetsmtpPort;
-            smtpClient.EnableSsl = SetIsSSL; // Aktivieren Sie SSL, wenn Ihr Server dies erfordert
+            smtpClient.Port =  ( SetIsSSL ) ? 587 : 465 ;
+            smtpClient.EnableSsl = SetIsSSL; // Activate SSL, if necessary
             smtpClient.Credentials = new NetworkCredential(SetEmailFrom, SetPassword);
             
 
-            // Expect100Continue auf false setzen
-            ServicePointManager.Expect100Continue = true;  // wenn eigener Server, und es schneller gehen darf: false;  Das true ist wichtig bei strato
+            // Expect100Continue = true needed for some provider, like strato.de
+            ServicePointManager.Expect100Continue = true;  // if you have your own server, and want it faster: false; Might try with your provider.
 
-            // TLS 1.2 als Sicherheitsprotokoll verwenden
+            // TLS 1.2 as Security protocol, might change in future
             ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
 
 
-            // E-Mail senden
-            try
+            // Send E-Mail , when nothing went wrong so far
+            if (strResult == "")
             {
-                smtpClient.Send(mail);
-                Console.WriteLine("E-Mail wurde erfolgreich gesendet!");
-                strResult = "Gesendet";
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Fehler beim Senden der E-Mail: " + ex.Message);
-                strResult = "Fehler beim Senden: " + ex.Message;
+                try
+                {
+                    smtpClient.Send(mail);
+                    Console.WriteLine("Yes, we did it!");
+                    strResult = "Ok";
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Sorry, we didn't make it: " + ex.Message);
+                    strResult = "Sorry, we didn't make it: " + ex.Message;
+                }
+
             }
 
-            // Aufräumen
+            // CleanUp
             mail.Dispose();
             smtpClient.Dispose();
 
             return strResult;
 
+
+            // ****************************************
+            // C|:-)  www.pdo.digital says Thank you !
+            // ****************************************
+
+        }
+
+
+
+        static bool IsValidEmail(string email)
+        {
+            try
+            {
+                var addr = new MailAddress(email);
+                return addr.Address == email;
+            }
+            catch
+            {
+                return false;
+            }
         }
     }
 }
